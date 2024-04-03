4e84324809d6ab4f05111956356a61714c57d9c3
isyly <79649023+isyly@users.noreply.github.com>
2024-04-03 18:50:38 +0200

    feat: footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,32 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Bounded from "@/components/Bounded";
+import Logo from "@/components/Logo";
 
-export default async function Footer() {
+export default async function footer() {
   const client = createClient();
-
   const settings = await client.getSingle("settings");
 
   return (
-    <footer>
-      <Link href="/">{settings.data.site_title}</Link>
-      <p>
-        ©{new Date().getFullYear()} {settings.data.site_title}
-      </p>
-      <ul>
-        {settings.data.navigation.map(({ link, label }) => (
-          <li key={label}>
-            <PrismicNextLink field={link}>{label}</PrismicNextLink>
-          </li>
-        ))}
-      </ul>
-    </footer>
+    <Bounded as="footer" className="py-4 md:py-6 lg:py-8">
+      <div className="flex gap-4 items-center justify-between sm:flex-row flex-col">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <p className="text-xs">
+          ©{new Date().getFullYear()} {settings.data.site_title}
+        </p>
+        <ul className="flex">
+          {settings.data.navigation.map(({ link, label }) => (
+            <li key={label}>
+              <PrismicNextLink field={link} className="p-3">
+                {label}
+              </PrismicNextLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Bounded>
   );
 }
