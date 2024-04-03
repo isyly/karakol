d2b12797ba6010187b6cfdc829f60aa330175cf1
isyly <79649023+isyly@users.noreply.github.com>
2024-04-03 15:49:02 +0200

    feat: create hero slice and helper components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,6 +13,10 @@ const config: Config = {
         body: ["var(--font-nunito-sans)", "sans"],
         display: ["var(--font-nunito)", "serif"],
       },
+      colors: {
+        'red-orange': { DEFAULT: '#FB3232', 50: '#FEE6E6', 100: '#FED2D2', 200: '#FDAAAA', 300: '#FC8282', 400: '#FC5A5A', 500: '#FB3232', 600: '#F00505', 700: '#B90404', 800: '#820303', 900: '#4B0202', 950: '#2F0101' },
+      },
+
     },
   },
   plugins: [],
