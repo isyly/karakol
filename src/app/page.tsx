f65dd2179024df8eeb1e8eca16245bf11166d4d0
isyly <79649023+isyly@users.noreply.github.com>
2024-04-03 12:08:43 +0200

    feat: add fallback metadata to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="font-display text-5xl">
-      <h1>Hello world</h1>
+      <h1>Hello worldaaaa</h1>
     </main>
   );
 }
