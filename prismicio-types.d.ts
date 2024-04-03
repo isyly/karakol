d1f28410b12f5b8c3f68b886be6f815a3cea8463
isyly <79649023+isyly@users.noreply.github.com>
2024-04-03 14:31:33 +0200

    feat : add homepage

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,12 +4,7 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type HomepageDocumentDataSlicesSlice =
-  | CallToActionSlice
-  | TextWithImageSlice
-  | TestimonialsSlice
-  | FeaturesSlice
-  | HeroSlice;
+type HomepageDocumentDataSlicesSlice = never;
 
 /**
  * Content for homePage documents
